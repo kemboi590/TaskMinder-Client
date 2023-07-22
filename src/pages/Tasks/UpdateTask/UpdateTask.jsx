13312b0fd2dc416efdc32371fc14bdc554005295
kemboi590 <bkemboi590@gmail.com>
2023-07-22 18:11:18 +0300

    refactored updateTask into components

--- src/pages/Tasks/UpdateTask/UpdateTask.jsx
+++ src/pages/Tasks/UpdateTask/UpdateTask.jsx
@@ -1,21 +1,13 @@
 import React, { useEffect, useState } from "react";
+import "./updateTask.css";
 import Axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { apidomain } from "../../../utils/domain";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { toastStyles } from "../../../toastConfig";
-import { apidomain } from "../../../utils/domain";
 import { validateForm } from "./formValidation";
 import "react-toastify/dist/ReactToastify.css";
-
-// Import the components
-
-import TaskDescriptionInput from "./DescriptionInput";
-import TaskAssignTo from "./AssignTo";
-import TaskProgressSelect from "./ProgressSelect";
-import TaskDueDateInput from "./DueDateInput";
-import TaskPrioritySelect from "./PrioritySelect";
-import UpdateButtons from "./UpdateButtons";
-import TaskTitleInput from "./TitleInput";
 
 function UpdateTask({ setshowUpdateForm, task, fetchSingleTask }) {
   const [title, setTitle] = useState("");
@@ -40,8 +32,9 @@ function UpdateTask({ setshowUpdateForm, task, fetchSingleTask }) {
 
   const [users, setUsers] = useState([]);
   const userData = useSelector((state) => state.user.user);
+  // const navigate = useNavigate();
 
-  // Get request to fetch all users in the database
+  // get request to fetch all users in the database
   const getAllUsers = async () => {
     try {
       const response = await Axios.get(`${apidomain}/users`, {
@@ -58,8 +51,7 @@ function UpdateTask({ setshowUpdateForm, task, fetchSingleTask }) {
   useEffect(() => {
     getAllUsers();
   }, []);
-
-  // Closing the submit form
+  // clossing the submit form
   const handleClose = () => {
     setshowUpdateForm(false);
   };
@@ -95,13 +87,16 @@ function UpdateTask({ setshowUpdateForm, task, fetchSingleTask }) {
             },
           }
         );
+        // console.log(response.data.message);
         toast.success(response.data.message, toastStyles.success);
         fetchSingleTask();
       } catch (response) {
+        // alert("an error occured, please try again");
         toast.error(
-          "An error occurred, please try again later",
+          "an error occured, please try again later",
           toastStyles.error
         );
+        // console.log(response);
       }
     }
   };
@@ -110,60 +105,136 @@ function UpdateTask({ setshowUpdateForm, task, fetchSingleTask }) {
     <div className="create_task_page">
       <form name="update form">
         <div className="task_form">
-          {/* Task Title */}
-          <TaskTitleInput
-            title={title}
-            onChange={(e) => setTitle(e.target.value)}
-            error={errors.title}
-          />
+          <div>
+            <label className="task_title">Task Title</label>
+            <br />
+            <input
+              className="title_input"
+              type="text"
+              placeholder="your task title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              name="title"
+            />
+            {errors.title && <span className="errors">{errors.title}</span>}
+          </div>
           <br />
-
-          {/* Task Description */}
-          <TaskDescriptionInput
-            description={description}
-            onChange={(e) => setDescription(e.target.value)}
-            error={errors.description}
-          />
+          <div>
+            <label className="task_description">Description</label>
+            <br />
+            <textarea
+              className="description_input"
+              cols="30"
+              rows="10"
+              placeholder="your task description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              name="description"
+            ></textarea>
+            {errors.description && (
+              <span className="errors">{errors.description}</span>
+            )}
+          </div>
           <br />
-
-          {/* Task Assign To */}
-          <TaskAssignTo
-            users={users}
-            assignedTo={assigned_to}
-            onChange={(e) => setAssigned_to(e.target.value)}
-            error={errors.assigned_to}
-          />
+          <div>
+            <label className="task_assign_to">Assign Task To</label>
+            <br />
+            <div className="check_member">
+              {/* getting all users in the database */}
+              {users.map((user) => (
+                <React.Fragment key={user.user_id}>
+                  <input
+                    type="radio"
+                    value={user.user_id}
+                    name="assigned_to"
+                    onChange={(e) => setAssigned_to(e.target.value)}
+                  />
+                  <label>{user.username}</label>
+                </React.Fragment>
+              ))}
+            </div>
+            {errors.assigned_to && (
+              <span className="errors">{errors.assigned_to}</span>
+            )}
+          </div>
+          <div className="task_progress">
+            <label className="task_progress">Task Progress</label>
+            <br />
+            {/* select for task progress */}
+            <select
+              className="select_progress"
+              name="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+            >
+              <option value="Not Started">Not Started</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Completed">Completed</option>
+              <option value="Overdue">Overdue</option>
+            </select>
+            {errors.status && <span className="errors">{errors.status}</span>}
+          </div>
           <br />
-
-          {/* Task Progress */}
-          <TaskProgressSelect
-            status={status}
-            onChange={(e) => setStatus(e.target.value)}
-            error={errors.status}
-          />
+          <div>
+            <label className="task_dueDate">Due Date</label>
+            <br />
+            <input
+              type="date"
+              name="due_date"
+              className="dueDate_calender"
+              value={due_date}
+              onChange={(e) => setDue_date(e.target.value)}
+            />
+            {errors.due_date && (
+              <span className="errors">{errors.due_date}</span>
+            )}
+          </div>
           <br />
-
-          {/* Task Due Date */}
-          <TaskDueDateInput
-            dueDate={due_date}
-            onChange={(e) => setDue_date(e.target.value)}
-            error={errors.due_date}
-          />
+          <div>
+            <label className="task_priority">Priority</label>
+            <br />
+            <div className="radio_task">
+              <input
+                type="radio"
+                name="priority"
+                value="High"
+                className="radio_priority"
+                checked={priority === "High"}
+                onChange={(e) => setPriority(e.target.value)}
+              />
+              <label className="task_priority">High</label>
+              <input
+                type="radio"
+                name="priority"
+                value="Medium"
+                className="radio_priority"
+                checked={priority === "Medium"}
+                onChange={(e) => setPriority(e.target.value)}
+              />
+              <label className="task_priority">Medium</label>
+              <input
+                type="radio"
+                name="priority"
+                value="Low"
+                className="radio_priority"
+                checked={priority === "Low"}
+                onChange={(e) => setPriority(e.target.value)}
+              />
+              <label className="priority">Low</label>
+            </div>
+            {errors.priority && (
+              <span className="errors">{errors.priority}</span>
+            )}
+          </div>
           <br />
-
-          {/* Task Priority */}
-          <TaskPrioritySelect
-            priority={priority}
-            onChange={(e) => setPriority(e.target.value)}
-            error={errors.priority}
-          />
-          <br />
-
-          {/* Update Buttons */}
-          <UpdateButtons
-            handleClose={handleClose}
-            handleUpdate={handleUpdate}
-          />
+          <div className="updateBTN">
+            <button onClick={handleClose} className="updatetask">
+              Close
+            </button>
+            <button onClick={handleUpdate} className="updatetask">
+              Update Task
+            </button>
+          </div>
         </div>
       </form>
     </div>
